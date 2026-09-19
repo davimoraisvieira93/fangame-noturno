69201b7a82c5a6acde4419c5773c82c480ef234f
davi_fnaf_gamer <davimoraisvieira93@gmail.com>
2026-09-19 10:51:39 -0300

    beatbox freddy

--- js/main.js
+++ js/main.js
@@ -28,18 +28,19 @@
     UI.showScreen('menu-screen');
     loadingScreen.classList.add('hidden');
 
-    // Toca a música assim que o menu carregar (pode precisar de 1 clique na tela antes)
-  // Toca a música assim que o menu carregar
-    beatboxMusic.play().catch(() => {
-      console.log("Chrome bloqueou o áudio automático. Aguardando clique do jogador...");
-      
-      // Armadilha: no exato momento que você clicar na tela, a música liga à força
-      document.body.addEventListener('click', () => {
-        if (beatboxMusic.paused && menuScreen.style.display !== 'none') {
-          beatboxMusic.play().catch(() => {});
-        }
-      }, { once: true });
-    });
+    // Adiciona um pequeno delay de 200ms para o navegador respirar antes de dar o play
+    setTimeout(() => {
+      beatboxMusic.play().catch(() => {
+        console.log("Chrome bloqueou o áudio automático. Aguardando clique do jogador...");
+        
+        // Armadilha: no exato momento que você clicar na tela, a música liga à força
+        document.body.addEventListener('click', () => {
+          if (beatboxMusic.paused && menuScreen.style.display !== 'none') {
+            beatboxMusic.play().catch(() => {});
+          }
+        }, { once: true });
+      });
+    }, 200);
 
     wireMenuButtons();
     wireOfficeControls();
@@ -50,9 +51,10 @@
 
   function wireMenuButtons() {
     document.getElementById('btn-start').addEventListener('click', () => {
-      // Para o beatbox na hora que o jogo começa
+      // Para o beatbox com força total, zera o tempo e muta para não vazar na Noite 1
       beatboxMusic.pause();
       beatboxMusic.currentTime = 0; 
+      beatboxMusic.muted = true;
       
       game.startNight(0);
     });
@@ -81,14 +83,16 @@
       game.startNight(game.nightIndex);
     });
     document.getElementById('btn-menu-gameover').addEventListener('click', () => {
-      beatboxMusic.play(); // Volta a tocar quando vai pro menu
+      beatboxMusic.muted = false; // Desmuta e volta a tocar quando vai pro menu
+      beatboxMusic.play();
       UI.showScreen('menu-screen');
     });
     document.getElementById('btn-next-night').addEventListener('click', () => {
       game.startNight(game.nightIndex + 1);
     });
     document.getElementById('btn-menu-victory').addEventListener('click', () => {
-      beatboxMusic.play(); // Volta a tocar quando vai pro menu
+      beatboxMusic.muted = false; // Desmuta e volta a tocar quando vai pro menu
+      beatboxMusic.play();
       UI.showScreen('menu-screen');
     });
   }
